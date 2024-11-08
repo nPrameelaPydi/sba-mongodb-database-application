c0ef30d6c4a304e4ac8634648b4ef8dc5043276d
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-07 18:29:18 -0800

    GET route to fetch all users

--- routes/user.js
+++ routes/user.js
@@ -31,4 +31,15 @@ router.post('/', async (req, res) => {
     }
 });
 
+// GET route to fetch all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+    }
+});
+
 export default router;
